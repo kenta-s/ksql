29783fcb7b8faea578e624765dc2150c3761a890
kenta-s <knt01222@gmail.com>
2017-09-12 09:23:26 +0900

    Add SyntaxTree

--- SQLParsers/Parser.hs
+++ SQLParsers/Parser.hs
@@ -4,8 +4,26 @@ module SQLParsers.Parser
 
 import Data.Char
 
+data SyntaxTree = SyntaxTree {
+    targetList :: [String],
+    fromClause :: [String]
+} deriving (Show)
+
 parse :: String -> String
 parse sql
-    | firstWord == "SELECT" = "SQL received. This function is not implemented yet."
+    | firstWord == "SELECT" = show tree
     | otherwise = error "Invalid SQL detected"
     where firstWord = [toUpper x | x <- takeWhile ( /= ' ' ) sql]
+          targets = extractTargets sql
+          tree = insertTarget targets $ SyntaxTree {targetList=[], fromClause=[]}
+
+insertTarget :: [String] -> SyntaxTree -> SyntaxTree
+insertTarget targets tree =
+    SyntaxTree {targetList = newTargetList, fromClause = newFromClause}
+    where newTargetList = targetList tree ++ targets
+          newFromClause = fromClause tree
+
+extractTargets :: String -> [String]
+-- TODO: fix the behavior
+extractTargets sql =
+    [[x | x <- dropWhile (/= ' ') sql]]
